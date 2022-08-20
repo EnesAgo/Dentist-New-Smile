c5b330b294943e2a7e4c29d98e578172279e6941
EnesAgo <enesago010@gmail.com>
2022-08-20 18:23:47 +0200

    fixing preventDefault

--- frontend/src/pages/Login/parts/LoginForm.js
+++ frontend/src/pages/Login/parts/LoginForm.js
@@ -4,7 +4,7 @@ import './../../../css/login.css'
 function LoginForm({ buttonEvent, getPassword, getUsername }) {
       return (
           <div className='LoginFormDiv'>
-              <form>
+              <form onSubmit={(e) => e.preventDefault()}>
                 <input onChange={getUsername} className='loginInput' type="text" placeholder='username' required /> <br />
                 <input onChange={getPassword} className='loginInput' type="password" placeholder='password' required /> <br />
                 <button className='submitButton' onClick={ buttonEvent }>submit</button>
